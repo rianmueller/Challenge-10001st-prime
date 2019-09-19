1fd011f97ccec138fe4a9add3868fb7e865701e3
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-09-18 18:53:21 -1000

    resolves all tests

--- prime-finder.js
+++ prime-finder.js
@@ -1,3 +1,22 @@
 module.exports = function(n) {
-  // do work here
+  let count = 0;
+  let number = 2;
+
+  function isPrime(number) {
+    for (i = 2; i < number / 2 + 1; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  while (count < n) {
+    if (isPrime(number) === true) {
+      count++;
+    }
+    number++;
+  }
+
+  return number - 1;
 };
